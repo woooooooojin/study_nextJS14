1d0097549e2a94e8ed6cc7683ce133f376347449
woooooooojin <kevin2205@naver.com>
2024-03-21 21:25:49 +0900

    css module

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,12 +1,13 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import styles from "../styles/navigation.module.css"
 
 export default function Navigation() {
     const path = usePathname();
 
   return (
-    <nav>
+    <nav className={styles.nav}>
         <ul>
             <li><Link href="/">Home</Link></li>{path === '/' ? '@' : ""}
             <li><Link href="/about-us">About Us</Link></li>{path === '/about-us' ? '@' : ""}
