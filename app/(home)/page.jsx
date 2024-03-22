424fbd5e9d17351772370d4ba45bd7d31e398850
woooooooojin <kevin2205@naver.com>
2024-03-22 12:22:36 +0900

    ...

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -7,7 +7,6 @@ export const metadata = {
 export const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies'
 
 async function getMovies(){
-    await new Promise((resolve)=>setTimeout(resolve,1000))
     const response =await fetch(URL)
     const json = await response.json()
     return json
