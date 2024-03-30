28c7af0c7a04331835f726c6787cb77cfab35be7
woooooooojin <kevin2205@naver.com>
2024-03-30 09:42:21 +0900

    prefetch

--- components/movie.jsx
+++ components/movie.jsx
@@ -14,7 +14,7 @@ export default function Movie({title, id, poster_path}) {
     <>
         <div className={styles.movie}>
             <img src={poster_path} alt={title} onClick={onClick}/>
-            <Link href={`/movies/${id}`}>{title}</Link>
+            <Link prefetch href={`/movies/${id}`}>{title}</Link>
         </div>
     </>
   )
